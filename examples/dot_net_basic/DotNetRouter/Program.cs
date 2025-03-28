52b253d265b32c65d0e08f78ddfec9b1547b9dcf
iontodirel <ion.todirel@gmail.com>
2025-03-27 18:34:06 -0700

    .net/c# sample

---
+++ examples/dot_net_basic/DotNetRouter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DotNetRouter
+{
+    internal class Program
+    {
+        [DllImport("RouterDll.dll", EntryPoint = "try_route_packet", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool TryRoutePacket(string packet, string routerCallsign, string routerPath, StringBuilder routedPacket, out int bufferSize);
+        
+        public static string RoutePacket(string packet, string routerCallsign, string routerPath)
+        {
+            int bufferSize = 1024;
+            StringBuilder routedPacketBuilder = new StringBuilder(bufferSize);
+            if (TryRoutePacket(packet, routerCallsign, routerPath, routedPacketBuilder, out bufferSize))
+            {
+                return routedPacketBuilder.ToString(0, bufferSize);
+            }
+            else
+            {
+                throw new Exception("Routing failed.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string routedPacket = RoutePacket("N0CALL>APRS,WIDE1-1,WIDE2-1:data", "DIGI", "WIDE1-1,WIDE2-2");
+            Console.WriteLine(routedPacket);
+        }
+    }
+}
